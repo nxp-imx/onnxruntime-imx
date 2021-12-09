262e312351e90fed9eafc21cb8184216f15f9e26
Pavel Macenauer <pavel.macenauer@nxp.com>
2021-12-09 12:41:32 +0100

    [AIR-6144] Copy VSI NPU EP
    Signed-off-by: Pavel Macenauer <pavel.macenauer@nxp.com>

---
+++ onnxruntime/core/providers/vsi_npu/vsi_npu_norm_op.h
@@ -0,0 +1,72 @@
+/****************************************************************************
+ *
+ *    Copyright (c) 2021 Vivante Corporation
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a
+ *    copy of this software and associated documentation files (the "Software"),
+ *    to deal in the Software without restriction, including without limitation
+ *    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *    and/or sell copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following conditions:
+ *
+ *    The above copyright notice and this permission notice shall be included in
+ *    all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *    DEALINGS IN THE SOFTWARE.
+ *
+ *****************************************************************************/
+#pragma once
+#include "vsi_npu_ort_interpreter.h"
+
+namespace onnxruntime {
+
+class VsiOpCallbackInfoLRN : public VsiOpCallbackInfo {
+   public:
+    VsiOpCallbackInfoLRN(){};
+    nnrt::op::OperationPtr createOperationPtr() override {
+        return std::make_shared<nnrt::op::LocalResponseNormOperation>();
+    };
+    void SetupAttribute(nnrt::op::OperationPtr op,
+                        const Node* node,
+                        ModelShellPtr& model,
+                        const onnxruntime::GraphViewer* graph_viewer) override;
+};
+
+class VsiOpCallbackInfoInstanceNormalization : public VsiOpCallbackInfo {
+   public:
+    VsiOpCallbackInfoInstanceNormalization(){};
+    void Setup(const Node*, ModelShellPtr&, const onnxruntime::GraphViewer*) override;
+    Status Compute(FunctionState state,
+                   const OrtApi* api,
+                   OrtKernelContext* context,
+                   NodeIndex node_index) override;
+};
+
+class VsiOpCallbackInfoBatchNormalization : public VsiOpCallbackInfo {
+   public:
+    VsiOpCallbackInfoBatchNormalization(){};
+    void Setup(const Node*, ModelShellPtr&, const onnxruntime::GraphViewer*) override;
+    Status Compute(FunctionState state,
+                   const OrtApi* api,
+                   OrtKernelContext* context,
+                   NodeIndex node_index) override;
+    bool IsNodeSupported(const onnxruntime::GraphViewer&, const Node*, std::string&) override;
+};
+
+#define MAP_OP_COMMON(name)                                                                    \
+    class VsiOpInfo##name : public VsiOpInfo {                                                 \
+       public:                                                                                 \
+        VsiOpInfo##name() { SetupCallbackInfo(std::make_shared<VsiOpCallbackInfo##name>()); }; \
+    };
+
+MAP_OP_COMMON(LRN)
+MAP_OP_COMMON(InstanceNormalization)
+MAP_OP_COMMON(BatchNormalization)
+
+}  // namespace onnxruntime
