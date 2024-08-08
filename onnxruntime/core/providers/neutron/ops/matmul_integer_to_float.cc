679f8fb3b7406d58f6ef95aa054e1f8f866c6ecf
Jiri Ocenasek (nxf96559) <jiri.ocenasek@nxp.com>
2024-08-08 15:46:11 +0800

    Initial implementation of MatMulIntegerToFloat on neutron
    Signed-off-by: Jiri Ocenasek (nxf96559) <jiri.ocenasek@nxp.com>

--- onnxruntime/core/providers/neutron/ops/matmul_integer_to_float.cc
+++ onnxruntime/core/providers/neutron/ops/matmul_integer_to_float.cc
@@ -22,6 +22,14 @@
 namespace onnxruntime {
 namespace neutron {
 
+#define CONST_BIAS (1 << 30)
+
+#ifndef NDEBUG
+extern double time_diff(struct timespec start_time, struct timespec end_time);
+#endif
+
+extern std::shared_ptr<NeutronStackAllocator> neutronAlloc;
+
 ONNX_OPERATOR_TYPED_KERNEL_EX(                                              \
     MatMulIntegerToFloat,                                                   \
     kMSDomain,                                                              \
@@ -183,45 +191,254 @@ void MatMulIntegerToFloat::FixupScaleTensor(const Tensor*& a_scale_tensor, const
   }
 }
 
+Status MatMulIntegerToFloat::PrePack(const Tensor& tensor, int input_idx, AllocatorPtr alloc,
+                                     /*out*/ bool& is_packed,
+                                     /*out*/ PrePackedWeights* prepacked_weights) {
+ try {
+    if (!useCPU) {
+      switch (input_idx) {
+      case IN_A:
+        break;
+      case IN_B:
+        {
+          m_b_rows = tensor.Shape()[1];
+          m_b_cols = tensor.Shape()[0];
+          m_handle = neutronAlloc->getMemoryHandle();
+          m_header = (uint32_t*) neutronAlloc->Alloc(16*sizeof(uint32_t), m_handle);
+          m_b_neutron = (int8_t*) neutronAlloc->Alloc(m_b_rows * m_b_cols, m_handle);
+          const int8_t *b_data = static_cast<const int8_t*>(tensor.DataRaw());
+          for(uint32_t i=0; i<m_b_cols; i++) {
+            for(uint32_t j=0; j<m_b_rows; j++) {
+              m_b_neutron[m_b_cols*j+i] = b_data[m_b_rows*i+j];
+            }
+          }
+        }
+        break;
+      case IN_A_SCALE:
+        {
+          m_a_scale_data = *(tensor.Data<float>());
+        }
+        break;
+      case IN_B_SCALE:
+        {
+          m_b_scale_data = tensor.Data<float>();
+        }
+        break;
+      case IN_A_ZERO_POINT:
+        {
+          m_a_zp = *(static_cast<const uint8_t*>(tensor.DataRaw()));
+          // Neutron expects the bias as a parameter anyway.
+          m_b_bias = (int32_t *)neutronAlloc->Alloc(m_b_rows*sizeof(int32_t), m_handle);
+
+          for (uint32_t i=0; i< m_b_rows; i++){
+            int32_t row_sum = 0;
+            for (uint32_t j=0; j< m_b_cols; j++) {
+              row_sum += *(m_b_neutron + i * m_b_cols + j);
+            }
+            m_b_bias[i] = (int32_t)( - row_sum * m_a_zp + CONST_BIAS);
+          }
+
+          m_b_factors = (uint32_t *)neutronAlloc->Alloc(m_b_rows*sizeof(uint32_t), m_handle);
+          float scale = 1;
+          for (uint32_t i=0; i< m_b_rows; i++){                                                                                  
+            float *pfloat = &scale;                                                                               
+            uint32_t u32 = *(uint32_t*) pfloat;                                                                                
+
+            uint32_t scaler = (u32 >>8) & 0x7fff ; // extract mantissa (15bits)                                                
+            int8_t exp_tmp = (u32 >> 23) & 0xff; // extract exponent                                                           
+
+            scaler = (exp_tmp==0) ? 0 :  scaler | 0x8000; // add hidden bit or zero out (if zero or subnormal                  
+            exp_tmp = -(exp_tmp -142); // we subtract FP32 offset as well as 16bit growth of our scaler (126 is power of -1 so mantissa is in range 0.5 to 1, 126 + 16=142, where 16 is the factor we multiply by in scaler)
+            int8_t exp = (exp_tmp>63) ? 63 : exp_tmp; // ensure that we don't exceed available shift bits (note that this step could, in theory be skipped if this never happens. Not sure if we can take the chance) 
+            //if (exp == 63)                                                                                                   
+            //      printf("scalar %0d, exp %0d", (uint32_t)scaler, (uint32_t)exp);                                            
+            scaler = (exp<<16) | scaler; // merge scaler and downshift factor into the Neutron 32bit scaler format (16bit scaler in LSB and then 6bits of downshift)
+
+            m_b_factors[i] = scaler;
+          }
+        }
+        break;
+      case IN_B_ZERO_POINT:
+        // we assume B has ZP equal to 0
+        // todo: implement a check
+        break;
+      case IN_BIAS:
+        {
+          m_output_bias = tensor.Data<float>();
+        }      
+        break;
+      }
+    }
+  }
+  catch (const std::bad_alloc &e) {
+    // Do not delegate this instance if out of memory
+#ifndef NDEBUG
+    printf("[MatMulIntegerToFloat] Unable to alocate Neutron memory\n");
+#endif
+    useCPU = true;
+  }
+  return Status::OK();
+}
+
 Status MatMulIntegerToFloat::Compute(OpKernelContext* ctx) const {
+  struct timespec t1, t2, t3, t4, t5;
+  
+#ifndef NDEBUG
+  printf("MatMulIntegerToFloat::Compute\n");
+#endif
+  
   const Tensor* a = ctx->Input<Tensor>(IN_A);
   const Tensor* b = packed_b_ ? nullptr : ctx->Input<Tensor>(IN_B);
 
-  const Tensor* a_scale_tensor = ctx->Input<Tensor>(IN_A_SCALE);
-  const Tensor* b_scale_tensor = ctx->Input<Tensor>(IN_B_SCALE);
-  FixupScaleTensor(a_scale_tensor, b_scale_tensor);
-  bool is_a_scale_scalar = IsScalarOr1ElementVector(a_scale_tensor);
-  bool is_b_scale_supported = IsBQuantParamSupported(b_scale_tensor->Shape(), nullptr != b ? b->Shape() : b_shape_);
+  if (!useCPU && m_header && m_b_neutron && m_b_bias && m_b_factors && m_b_scale_data) {
+    
+    clock_gettime(CLOCK_REALTIME, &t1);
 
-  // validate zero point of a
-  uint8_t a_zero_point = 0;
-  const Tensor* a_zero_point_tensor = ctx->Input<Tensor>(IN_A_ZERO_POINT);
-  if (a_zero_point_tensor != nullptr) {
-    ORT_ENFORCE(IsScalarOr1ElementVector(a_zero_point_tensor),
-                "MatMulIntegerToFloat : input a zero point must be a scalar or 1D tensor of size 1. Per-Channel is not supported yet.");
-    a_zero_point = *(static_cast<const uint8_t*>(a_zero_point_tensor->DataRaw()));
+    neutronAlloc->pushMemoryState(m_handle);
+
+    // non-transposed b
+    uint32_t neutron_a_rows = a->Shape()[1];
+    uint32_t neutron_a_cols = a->Shape()[2];
+    uint32_t neutron_b_rows = b ? b->Shape()[1] : m_b_rows;
+    uint32_t neutron_b_cols = b ? b->Shape()[0] : m_b_cols;
+    if (neutron_a_cols != neutron_b_cols) {
+      printf("Neutron dimenssions do not match!\n");
+    }
+    
+    clock_gettime(CLOCK_REALTIME, &t2);
+
+    uint32_t a_size = neutron_a_rows * neutron_a_cols;
+    uint8_t *a_neutron = (uint8_t *) neutronAlloc->AllocReserved(a_size*sizeof(uint8_t), m_handle);
+    auto  a_data = static_cast<const uint8_t*>(a->DataRaw());
+    memcpy(a_neutron, a_data, a_size);
+
+    clock_gettime(CLOCK_REALTIME, &t3);
+
+    uint32_t *y_neutron = (uint32_t *) neutronAlloc->AllocReserved(neutron_a_rows * neutron_b_rows * sizeof(uint32_t), m_handle);
+    
+    m_header[0] = 0;
+    m_header[1] = 0;
+    m_header[2] = neutron_a_rows;
+    m_header[3] = neutron_a_cols;
+    m_header[4] = neutron_b_rows;
+    m_header[5] = (uint8_t *)a_neutron - (uint8_t *)m_header;
+    m_header[6] = (uint8_t *)m_b_neutron - (uint8_t *)m_header;
+    m_header[7] = (uint8_t *)m_b_bias - (uint8_t *)m_header;
+    m_header[8] = (uint8_t *)m_b_factors - (uint8_t *)m_header;
+    m_header[9] = (uint8_t *)y_neutron - (uint8_t *)m_header;
+    m_header[10] = 0; // m_y_zp;
+    m_header[11] = 4; // result num bytes
+
+    NeutronError ret = ENONE;
+    ret = matmul((const void *)m_header, 0, m_handle, 0, 0, 0, 0);
+    if (ret != ENONE){
+        printf("matmul() error %d\n", ret);
+        return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "matmul() error");
+    }
+
+    Tensor* y = ctx->Output(OUT_Y, {1, neutron_a_rows, neutron_b_rows});
+    float* y_data = static_cast<float*>(y->MutableDataRaw());
+    
+    clock_gettime(CLOCK_REALTIME, &t4);
+
+    uint32_t* y_new = (uint32_t*) malloc(neutron_a_rows * neutron_b_rows * sizeof(uint32_t));
+    memcpy(y_new, y_neutron, neutron_a_rows * neutron_b_rows * sizeof(uint32_t));
+    // The matmul result is internally unsigned but we assume only 31 bits are used.
+    int32_t* input = (int32_t*) y_new; // y_neutron;
+    auto* output = y_data;
+    for (uint32_t i=0; i<static_cast<uint32_t>(neutron_a_rows); i++) {
+      auto* b_scale = m_b_scale_data;
+      if (m_output_bias) {
+        auto* bias = m_output_bias;
+        for (uint32_t j=0; j<static_cast<uint32_t>(neutron_b_rows); j++) {
+          *output++ = static_cast<float>(*bias++) + static_cast<float>(static_cast<int32_t>(*input++) - static_cast<int32_t>(CONST_BIAS)) * static_cast<float>(*b_scale++) * m_a_scale_data;
+        }
+      } else {
+        for (uint32_t j=0; j<static_cast<uint32_t>(neutron_b_rows); j++) {
+          *output++ = static_cast<float>(static_cast<int32_t>(*input++) - static_cast<int32_t>(CONST_BIAS)) * static_cast<float>(*b_scale++) * m_a_scale_data;
+        }
+      }
+    }
+    free(y_new);
+    
+
+#ifndef NDEBUG
+    printf("\nA shape=%ld %ld %ld\n\n",a->Shape()[0],a->Shape()[1],a->Shape()[2]);
+    printf("\n");
+    for (uint32_t i=0; i<neutron_a_rows; i++) {
+      for (uint32_t j=0; j<neutron_b_rows; j++) {
+        printf("C[%d][%d]=%f ",i,j,y_data[i * neutron_b_rows + j] * 1.0);
+      }
+      printf("\n");
+    }
+#endif
+    
+    neutronAlloc->popMemoryState(m_handle);
+    clock_gettime(CLOCK_REALTIME, &t5);
+    
+#ifndef NDEBUG
+    printf("Neutron: Computed MatMulIntegerToFloat in %f us\n", time_diff(t1,t4));
+    printf("Neutron: Postprocessed MatMulIntegerToFloat in %f us\n", time_diff(t4,t5));
+#endif
   }
+#ifdef NDEBUG
+  else
+#endif
+  {
+    clock_gettime(CLOCK_REALTIME, &t1);
+    const Tensor* a_scale_tensor = ctx->Input<Tensor>(IN_A_SCALE);
+    const Tensor* b_scale_tensor = ctx->Input<Tensor>(IN_B_SCALE);
+    FixupScaleTensor(a_scale_tensor, b_scale_tensor);
+    bool is_a_scale_scalar = IsScalarOr1ElementVector(a_scale_tensor);
+    bool is_b_scale_supported = IsBQuantParamSupported(b_scale_tensor->Shape(), nullptr != b ? b->Shape() : b_shape_);
 
-  const Tensor* b_zp_tensor = ctx->Input<Tensor>(IN_B_ZERO_POINT);
-  ORT_RETURN_IF_ERROR(ComputeCommon(
-      ctx,
-      static_cast<const uint8_t*>(a->DataRaw()),
-      a->Shape(),
-      is_a_scale_scalar ? *a_scale_tensor->Data<float>() : 1.f,
-      a_zero_point,
-      a->IsDataType<int8_t>(),
-      b,
-      is_b_scale_supported ? b_scale_tensor : nullptr,
-      b_zp_tensor,
-      ctx->Input<Tensor>(IN_BIAS)));
+    // validate zero point of a
+    uint8_t a_zero_point = 0;
+    const Tensor* a_zero_point_tensor = ctx->Input<Tensor>(IN_A_ZERO_POINT);
+    if (a_zero_point_tensor != nullptr) {
+      ORT_ENFORCE(IsScalarOr1ElementVector(a_zero_point_tensor),
+                  "MatMulIntegerToFloat : input a zero point must be a scalar or 1D tensor of size 1. Per-Channel is not supported yet.");
+      a_zero_point = *(static_cast<const uint8_t*>(a_zero_point_tensor->DataRaw()));
+    }
 
-  if (!is_a_scale_scalar) {
-    ScaleOutput(*a_scale_tensor, *ctx->Output<Tensor>(0));
+    const Tensor* b_zp_tensor = ctx->Input<Tensor>(IN_B_ZERO_POINT);
+    ORT_RETURN_IF_ERROR(ComputeCommon(
+                                      ctx,
+                                      static_cast<const uint8_t*>(a->DataRaw()),
+                                      a->Shape(),
+                                      is_a_scale_scalar ? *a_scale_tensor->Data<float>() : 1.f,
+                                      a_zero_point,
+                                      a->IsDataType<int8_t>(),
+                                      b,
+                                      is_b_scale_supported ? b_scale_tensor : nullptr,
+                                      b_zp_tensor,
+                                      ctx->Input<Tensor>(IN_BIAS)));
+
+    if (!is_a_scale_scalar) {
+      ScaleOutput(*a_scale_tensor, *ctx->Output<Tensor>(0));
+    }
+    if (!is_b_scale_supported) {
+      ScaleOutput(*b_scale_tensor, *ctx->Output<Tensor>(0));
+    }
+
+#ifndef NDEBUG
+    // Dump the output
+    const Tensor* y = ctx->Output<Tensor>(0);
+    printf("\nY shape=%ld %ld %ld\n\n",y->Shape()[0],y->Shape()[1],y->Shape()[2]);
+    const float *y_data = static_cast<const float*>(y->DataRaw());
+    printf("\n");
+    for (int i=0; i<y->Shape()[1]; i++) {
+      for (int j=0; j<y->Shape()[2]; j++) {
+        printf("Y[%d][%d]=%f ",i,j,y_data[i * y->Shape()[2] + j]);
+      }
+      printf("\n");
+    }
+#endif
+    clock_gettime(CLOCK_REALTIME, &t4);
+#ifndef NDEBUG
+    printf("CPU: Computed MatMulIntegerToFloat in %f us\n", time_diff(t1,t4));
+#endif
   }
-  if (!is_b_scale_supported) {
-    ScaleOutput(*b_scale_tensor, *ctx->Output<Tensor>(0));
-  }
-
   return Status::OK();
 }
 
