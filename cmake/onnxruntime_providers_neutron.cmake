18e2189cdfefe3a50ca0af13fd919bfc40eb3166
Allen An <pinghua.an@nxp.com>
2025-04-04 08:24:41 +0800

    Fixed onnxruntime_providers_neutron build issues
    Signed-off-by: Allen An <pinghua.an@nxp.com>

--- cmake/onnxruntime_providers_neutron.cmake
+++ cmake/onnxruntime_providers_neutron.cmake
@@ -9,7 +9,7 @@ file(GLOB_RECURSE onnxruntime_providers_neutron_srcs
 source_group(TREE ${ONNXRUNTIME_ROOT}/core FILES ${onnxruntime_providers_neutron_srcs})
 onnxruntime_add_static_library(onnxruntime_providers_neutron ${onnxruntime_providers_neutron_srcs})
 onnxruntime_add_include_to_target(onnxruntime_providers_neutron
-  onnxruntime_common onnxruntime_framework onnx onnx_proto ${PROTOBUF_LIB} flatbuffers::flatbuffers Boost::mp11 safeint_interface
+  onnxruntime_common onnxruntime_framework onnx onnx_proto ${PROTOBUF_LIB} flatbuffers::flatbuffers Boost::mp11 safeint_interface Eigen3::Eigen
 )
 
 set(NEUTRON_DRIVER_LIB "NeutronDriver")
