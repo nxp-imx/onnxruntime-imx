b949157ea3f5c578a2c0070565d7beff3ad7688a
Feng Guo <feng.guo@nxp.com>
2025-04-30 15:31:09 +0800

    Put unsupported MatMulNBits nodes to CPU
    Signed-off-by: Feng Guo <feng.guo@nxp.com>

--- onnxruntime/core/providers/neutron/neutron_execution_provider.h
+++ onnxruntime/core/providers/neutron/neutron_execution_provider.h
@@ -14,6 +14,12 @@ class NeutronExecutionProvider : public IExecutionProvider {
 
   std::shared_ptr<KernelRegistry> GetKernelRegistry() const override;
 
+  std::vector<std::unique_ptr<ComputeCapability>>
+  GetCapability(const onnxruntime::GraphViewer& graph_viewer,
+                const IKernelLookup& /*kernel_lookup*/,
+                const GraphOptimizerRegistry& /* graph_optimizer_registry */,
+                IResourceAccountant* /* resource_accountant */) const override;
+
   const void* GetExecutionHandle() const noexcept override {
     return nullptr;
   }
