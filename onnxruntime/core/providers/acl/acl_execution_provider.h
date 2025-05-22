4bc41321cf790ed27824d1488fbc3d1afef8144a
Allen An <pinghua.an@nxp.com>
2025-05-22 16:48:47 +0800

    Updated the copyrights License Info
    Signed-off-by: Allen An <pinghua.an@nxp.com>

--- onnxruntime/core/providers/acl/acl_execution_provider.h
+++ onnxruntime/core/providers/acl/acl_execution_provider.h
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Copyright (c) 2019, NXP Semiconductor, Inc. All rights reserved.
+// Copyright 2025 NXP
 // SPDX-FileCopyrightText: Copyright 2024 Arm Limited and/or its affiliates <open-source-office@arm.com>
 // Licensed under the MIT License.
 
