4d988fa2091d6b1792dbb2277be40e212d105c5a
Feng Guo <feng.guo@nxp.com>
2025-07-24 11:32:06 +0800

    Neutron EP: Register custom OP for NeutronGraph generated by neutron-converter
    Signed-off-by: Feng Guo <feng.guo@nxp.com>

--- onnxruntime/core/session/environment.cc
+++ onnxruntime/core/session/environment.cc
@@ -37,6 +37,9 @@
 #ifdef USE_DML
 #include "core/graph/dml_ops/dml_defs.h"
 #endif
+#ifdef USE_NEUTRON
+#include "core/graph/neutron_ops/neutron_defs.h"
+#endif
 
 #include "core/platform/env.h"
 #include "core/util/thread_utils.h"
@@ -245,6 +248,9 @@ Status Environment::Initialize(std::unique_ptr<logging::LoggingManager> logging_
 #ifdef USE_DML
       domainToVersionRangeInstance.AddDomainToVersion(onnxruntime::kMSDmlDomain, 1, 1);
 #endif
+#ifdef USE_NEUTRON
+      domainToVersionRangeInstance.AddDomainToVersion(onnxruntime::kNeutronDomain, 1, 1);
+#endif
 // Register contributed schemas.
 // The corresponding kernels are registered inside the appropriate execution provider.
 #ifndef DISABLE_CONTRIB_OPS
@@ -257,6 +263,9 @@ Status Environment::Initialize(std::unique_ptr<logging::LoggingManager> logging_
 
 #ifdef USE_DML
       dml::RegisterDmlSchemas();
+#endif
+#ifdef USE_NEUTRON
+      neutron::RegisterNeutronSchemas();
 #endif
       RegisterOnnxOperatorSetSchema();
 
