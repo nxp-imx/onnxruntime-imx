5c4454850ca8719b1046cb4a35187d6e2e229abc
Joshua Puerta <joshua.puerta@nxp.com>
2025-11-07 13:46:07 +0800

    Add minimal infrastructure for neutron execution provider
    Signed-off-by: Allen An <pinghua.an@nxp.com>

---
+++ include/onnxruntime/core/providers/neutron/neutron_provider_factory.h
@@ -0,0 +1,24 @@
+// Copyright (c) NXP. All rights reserved.
+
+#pragma once
+
+#include "onnxruntime_c_api.h"
+
+
+enum NeutronFlags {
+  NEUTRON_FLAG_USE_NONE = 0x000,
+
+  NEUTRON_FLAG_LAST = NEUTRON_FLAG_USE_NONE,
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ORT_EXPORT ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_Neutron,
+                          _In_ OrtSessionOptions* options, uint32_t neutron_flags);
+
+#ifdef __cplusplus
+}
+#endif
+
